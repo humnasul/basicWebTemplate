50e13cb1a57daa8e621fb25755d1dbb2904d1248
Humna <111152681+humnasul@users.noreply.github.com>
2023-06-22 11:57:58 -0400

    Update script.js
    added start button functionality :)

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("buttons").style.display = "none";
+document.getElementById("option1").style.display = "none";
+
+function clickStart() {
+  document.getElementById("buttons").style.display = "block";
+  document.getElementById("start").style.display = "none";
+  //alert('send message to screen');
+}
