be605d34694a1a5de815b9a328a158ba323ab122
Humna <111152681+humnasul@users.noreply.github.com>
2023-06-30 23:33:48 -0400

    updated displays for clicking all buttons - new functions!

--- script2.js
+++ script2.js
@@ -1,5 +1,21 @@
 document.getElementById("option1").style.display = "none";
+document.getElementById("option2").style.display = "none";
+document.getElementById("option3").style.display = "none";
 
 function onClick1() {
   document.getElementById("option1").style.display = "block";
+  document.getElementById("option3").style.display = "none";
+  document.getElementById("option2").style.display = "none";
+}
+
+function onClick2() {
+  document.getElementById("option2").style.display = "block";
+  document.getElementById("option3").style.display = "none";
+  document.getElementById("option1").style.display = "none";
+}
+
+function onClick3() {
+  document.getElementById("option3").style.display = "block";
+  document.getElementById("option1").style.display = "none";
+  document.getElementById("option2").style.display = "none";
 }
